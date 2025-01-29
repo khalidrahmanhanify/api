a9b585e9ca7c2d421352bfd08da8c0fc62c0fd9f
khalidrahmanhanify <khalidrahmanhanify@gmail.com>
2025-01-29 23:34:08 +0430

    Integrate Firebase for storing cards

--- netlify/functions/uploadCard.js
+++ netlify/functions/uploadCard.js
@@ -4,7 +4,7 @@ const admin = require("firebase-admin");
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(
-      require("./firebase-service-account.json")
+      require("./../../firebase-service-account.json")
     ), // Path to the service account JSON file
   });
 }
